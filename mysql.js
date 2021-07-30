8c4f1c3c0f7e5db30d6cd1837ac4d0e8541ba82f
Facundo F <facundof13@gmail.com>
2021-07-30 13:42:58 -0400

    test

--- mysql.js
+++ mysql.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
     host: 'localhost',
-    user: 'root',
+    user: 'root@localhost',
     password: process.env.MYSQL_PASSWORD,
     database: 'final_project',
 })
