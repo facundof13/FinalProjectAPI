14b6bfcb4354267f6889c5b0c6afd4b3a1f22dd2
Facundo F <facundof13@gmail.com>
2021-07-30 22:54:12 -0400

    cleanup

--- mysql.js
+++ mysql.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: process.env.MYSQL_PASSWORD,
+    password: process.env.MYSQL_PASSWORD || 'root',
     database: 'final_project',
 })
 
